cdbd26e2c6d4624ed1940db32bbfeff2a10c49dd
adarsh456 <38810740+adarsh456@users.noreply.github.com>
2019-09-13 09:05:37 +0530

    Update GeocodeService.java

--- src/main/java/service/GeocodeService.java
+++ src/main/java/service/GeocodeService.java
@@ -18,66 +18,60 @@ import redis.clients.jedis.JedisPool;
 @Service
 public class GeocodeService {
 
-	public geodata getgeocode(String query) {
-		
-	JedisPool jedispool = RedisConfig.getJedisPool();
-    Jedis jedis = jedispool.getResource();
-    geodata g = new geodata();
+    public geodata getgeocode(String query) {
     
-    Set<String> names = jedis.keys(query);
-    
-    
-    if (names.size() == 0) {		//Check if Cache is available if yes
-    	
-    	
-    	//get data from API and store it in Cache for future API calls
+	    JedisPool jedispool = RedisConfig.getJedisPool();
+	    Jedis jedis = jedispool.getResource();
+	    geodata g = new geodata();
 
-    	//Call the Google GeoCode RestAPI 
-    	RestTemplate restTemplate = new RestTemplate();
-    	String tempquery = query.replaceAll(",","+");
-    	String ResourceUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+tempquery+"&key=AIzaSyBC1mXeLL9uJrgXQiv3CQDJyt92ivDNPs0";
-    	ResponseEntity<String> response = restTemplate.getForEntity(ResourceUrl, String.class);	
-    	
-    	//Jackson is used to read the JSON response given by Google API
-    	ObjectMapper mapper = new ObjectMapper();
-    	JsonNode root;
-    	try {
-    		root = mapper.readTree(response.getBody());
-    		JsonNode results = root.path("results");
-    		
-    		for (JsonNode result:results) {
-    			
-    			JsonNode loc = result.path("geometry").path("location");
-    			
-    			String lat = loc.path("lat").toString();	//latitude
-    			String long1 = loc.path("lng").toString();	//longitude
-    			
-    			String json = "{\"latitude\":\""+lat+"\",\"longitude\":\""+
-    							long1+"\",\"source\":\"CACHE\"}";	//store JSON as string in Cache
-    			
-    			jedis.set(query,json);						//store key -> value pair in cache
-    			
-    			g.setLatitude(lat);
-    			g.setLongitude(long1);
-    			g.setSource("API");
-    		}
-    		
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	
-    }else {
-    	//if data available get it from Cache and Deserialize JSON string -> object  (i.e Geodata)
-    	String geojson = jedis.get(query);
-    	
-    	try {
-            g = new ObjectMapper().readValue(geojson, geodata.class);	//Deserialize using Objectmapper
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-    
-	return g;	//Return Final GeoData i.e. Latitude,Longitude,Source
-	}
+	    Set<String> names = jedis.keys(query);
+
+
+	    if (names.size() == 0) {		//Check if Cache is available if yes get data from API and store it in Cache for future API calls
+
+		RestTemplate restTemplate = new RestTemplate();
+		String tempquery = query.replaceAll(",","+");   //Call the Google GeoCode RestAPI 
+		String ResourceUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+tempquery+"&key=AIzaSyBC1mXeLL9uJrgXQiv3CQDJyt92ivDNPs0";
+		ResponseEntity<String> response = restTemplate.getForEntity(ResourceUrl, String.class);	
+
+		//Jackson is used to read the JSON response given by Google API
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root;
+		try {
+			root = mapper.readTree(response.getBody());
+			JsonNode results = root.path("results");
+
+			for (JsonNode result:results) {
+
+				JsonNode loc = result.path("geometry").path("location");
+
+				String lat = loc.path("lat").toString();	//latitude
+				String long1 = loc.path("lng").toString();	//longitude
+
+				String json = "{\"latitude\":\""+lat+"\",\"longitude\":\""+
+								long1+"\",\"source\":\"CACHE\"}";	//store JSON as string in Cache
+				jedis.set(query,json);						//store key -> value pair in cache
+
+				g.setLatitude(lat);
+				g.setLongitude(long1);
+				g.setSource("API");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	    }else {
+		//if data available get it from Cache and Deserialize JSON string -> object  (i.e Geodata)
+		String geojson = jedis.get(query);
+		try {
+		    g = new ObjectMapper().readValue(geojson, geodata.class);	//Deserialize using Objectmapper
+		} catch (IOException e) {
+		    System.out.println(e);
+		}
+	    }
+
+   return g;	//Return Final GeoData i.e. Latitude,Longitude,Source
+   }
 	
 }
